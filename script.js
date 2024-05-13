564e6697b008119f1cccbb39d6e48de1328bb7b5
avi <avineshwarthegreat@gmail.com>
2024-05-13 21:06:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("b1").addEventListener("click", function () {
+  document.getElementById("registrationForm").classList.toggle("show");
+  document.getElementById("registrationForm").classList.toggle("hidden");
+});
